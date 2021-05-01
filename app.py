040f697e387b845a4d31605d0a783e7644043ff2
JoeyyScott <joey-scott@live.com>
2021-05-01 17:00:38 +0000

    [ADD] Added base structure for gamersaurus.html

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
+
 
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 @app.route("/")
-def hello():
-    return "Gamersaurus is here!"
+@app.route("/get_gs")
+def get_gs():
+    terms = mongo.db.thesaurus.find()
+    return render_template("gamersaurus.html", terms=terms)
 
 
 if __name__ == "__main__":
