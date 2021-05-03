95d95ea12cb3c077abd8b246c2bcca7ba445166b
JoeyyScott <joey-scott@live.com>
2021-05-03 03:16:14 +0000

    [ADD] Added functionality to check users login details

--- app.py
+++ app.py
@@ -49,6 +49,32 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Checking if user already exists in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # Ensure the hashed password matches data for username in DB
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # If the password is invalid
+                flash("Invalid login details")
+                return redirect(url_for("login"))
+
+        else:
+            # Username not found
+            flash("Invalid login details")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
