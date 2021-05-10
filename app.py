aa5ef977f6d115683fbb4bd24016c22947d97f3e
JoeyyScott <joey-scott@live.com>
2021-05-10 23:54:33 +0000

    [UPDATE] Updated contact.html validation to not use text area patterns but javascript to pass html validations

--- app.py
+++ app.py
@@ -79,11 +79,10 @@ def login():
             # Ensure the hashed password matches data for username in DB
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
-                        session["user"] = request.form.get("username").lower()
-                        flash("Successfully logged in as {}".format(
-                            request.form.get("username")))
-                        return redirect(url_for(
-                            "profile", username=session["user"]))
+                session["user"] = request.form.get("username").lower()
+                flash("Successfully logged in as {}".format(
+                    request.form.get("username")))
+                return redirect(url_for("profile", username=session["user"]))
             else:
                 # If the password is invalid
                 flash("Invalid login details")
--- static/js/script.js
+++ static/js/script.js
@@ -3,69 +3,58 @@ $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $(".collapsible").collapsible();
     $('.modal').modal();
-    // Credit for making textarea validate with Regex patterns
-    var errorMessage = "Please match the requested format.";
-
-    $( this ).find( "textarea" ).on( "input change propertychange", function() {
-
-        var pattern = $( this ).attr( "pattern" );
-
-        if(typeof pattern !== typeof undefined && pattern !== false)
-        {
-            var patternRegex = new RegExp( "^" + pattern.replace(/^\^|\$$/g, '') + "$", "g" );
-
-            hasError = !$( this ).val().match( patternRegex );
-
-            if ( typeof this.setCustomValidity === "function") 
-            {
-                this.setCustomValidity( hasError ? errorMessage : "" );
-            } 
-            else 
-            {
-                $( this ).toggleClass( "error", !!hasError );
-                $( this ).toggleClass( "ok", !hasError );
-
-                if ( hasError ) 
-                {
-                    $( this ).attr( "title", errorMessage );
-                } 
-                else
-                {
-                    $( this ).removeAttr( "title" );
-                }
-            }
-        }
-    });
     // Checking for input in the 2 password fields on register with credit for keyup and .val() - see README.md
     $('#password, #passwordconfirm').on('keyup', function () {
         if ($('#password').val() == $('#passwordconfirm').val()) {
-            $('#pwcheck').html('Match').css("color", '#f3ca40');
+            $('#msgPw').html('Match').css("color", '#f3ca40');
         } else
-            $('#pwcheck').html('No Match').css("color", '#c0960c');
+            $('#msgPw').html('No Match').css("color", '#c0960c');
     });
 });
 
 // Constants
 const form = document.getElementById('contactForm');
 const contactButton = document.getElementById('buttonContact');
-const formName = document.getElementById('formName');
-const formEmail = document.getElementById('formEmail');
 const formMessage = document.getElementById('formMessage');
+const msgError = document.getElementById('msgError');
 
 //Credit for submit event listener to detect form submission
-$( "#contactForm" ).submit(function( event ) { event.preventDefault(); contactUs(this); });
+$( "#contactForm" ).submit(function( event ) { event.preventDefault(); sendMail(this); });
 
-function contactUs(contactForm) {
-    // Credit for emailJS send function
-    emailjs.send("service_qtd8qrn", "gamersaurusContact", {
-        formName: formName.value,
-        formEmail: formEmail.value,
-        formMessage: formMessage.value,
-    })
-    .then(
-        //Changes the submitButton text to convey to the user if their submission was successful or not
-        function (response) { contactButton.innerHTML = `Thank you for your query! <i class="fas fa-smile-beam"></i>`; form.reset(); },
-        function (error) { contactButton.innerHTML = `Oops! Please try again. <i class="fas fa-frown"></i>`; }
-    );
-    //End credit for emailJS send function
+function stringCheck(x) {
+    if (RegExp(/^([a-zA-Z0-9_]+\s?){1,}\\*$/).test(x) === false) { return 'nomatch' }
+    else { return 'match' }
+}
+
+function sendMail() {
+    let errors = [];
+    let noFormMessage = (formMessage.value === null || formMessage.value === "");
+
+    // If no input detected display an error
+    if (noFormMessage) { errors.push("Message field required."); }
+
+    // If double spaces or symbols are detected display an error
+    if (!noFormMessage) {
+        if (stringCheck(formMessage.value) === 'nomatch') { errors.push('Message must contain letters, numbers and no double spaces/symbols.') }
+    }
+
+    // Clears previous error messages
+    $("#msgError").empty();
+    if (errors.length === 0) {
+        emailjs.send("service_qtd8qrn", "gamersaurusContact", {
+            formName: formName.value,
+            formEmail: formEmail.value,
+            formMessage: formMessage.value,
+        })
+        .then(
+            //Changes the submitButton text to convey to the user if their submission was successful or not
+            function (response) { contactButton.innerHTML = `Thank you for your query! <i class="fas fa-smile-beam"></i>`; form.reset(); },
+            function (error) { contactButton.innerHTML = `Oops! Please try again. <i class="fas fa-frown"></i>`; }
+        );
+
+    return false;  
+
+    } else { for (i = 0; i < errors.length; i++) { msgError.innerHTML = msgError.innerHTML + errors [i] + '<br>'; }
+    return false;
+    }
 }
